0ca7837edd3293410b6d50ca6204107ca8e40a59
Alejandro Duarte <alejandro@vaadin.com>
2018-07-11 19:15:28 +0300

    Updated to Vaadin 10

--- src/main/java/com/example/VaadinUI.java
+++ src/main/java/com/example/VaadinUI.java
@@ -1,43 +1,37 @@
 package com.example;
 
-import com.vaadin.data.Binder;
-import com.vaadin.server.VaadinRequest;
-import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.*;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.router.Route;
 
-import java.util.List;
+@Route("")
+public class VaadinUI extends VerticalLayout {
 
-@SpringUI
-public class VaadinUI extends UI {
-
-    @Autowired
-    private CustomerService service;
-
+    private final CustomerService service;
     private Customer customer;
 
-    private Binder<Customer> binder = new Binder<>(Customer.class);
-
-    private Grid<Customer> grid = new Grid(Customer.class);
+    private Grid<Customer> grid = new Grid<>(Customer.class);
     private TextField firstName = new TextField("First name");
     private TextField lastName = new TextField("Last name");
     private Button save = new Button("Save", e -> saveCustomer());
 
-    @Override
-    protected void init(VaadinRequest request) {
-        updateGrid();
+    public VaadinUI(CustomerService service) {
+        this.service = service;
+
         grid.setColumns("firstName", "lastName");
         grid.addSelectionListener(e -> updateForm());
 
-        binder.bindInstanceFields(this);
-
-        VerticalLayout layout = new VerticalLayout(grid, firstName, lastName, save);
-        setContent(layout);
+        add(grid, firstName, lastName, save);
+        setMargin(true);
+        setSpacing(true);
+        updateGrid();
     }
 
     private void updateGrid() {
-        List<Customer> customers = service.findAll();
-        grid.setItems(customers);
+        grid.setItems(service.findAll());
         setFormVisible(false);
     }
 
@@ -46,6 +40,8 @@ public class VaadinUI extends UI {
             setFormVisible(false);
         } else {
             customer = grid.asSingleSelect().getValue();
+            Binder<Customer> binder = new Binder<>(Customer.class);
+            binder.bindInstanceFields(this);
             binder.setBean(customer);
             setFormVisible(true);
         }
